af73acb67d0efbd0df33e81c6ff93f5bd0826198
chanchal <300656712+sharmacheeku192-oss@users.noreply.github.com>
2026-09-18 22:32:22 +0530

    Extract nucleotide information from ATP7B variant

--- scripts/variant_validation.py
+++ scripts/variant_validation.py
@@ -8,3 +8,13 @@ with open(variant_file, "r") as file:
 print(lines)
 data = lines[1].strip().split(",")
 print(data)
+dna_change = data[1]
+
+position = int(dna_change[2:6])
+reference_base = dna_change[6]
+alternate_base = dna_change[8]
+
+print("DNA change:", dna_change)
+print("Coding position:", position)
+print("Reference base:", reference_base)
+print("Alternate base:", alternate_base)
